02c1bc0a06aa05dfd2beb3b585f8d21b52586896
John Michihara <john.michihara@gmail.com>
2021-06-04 06:27:32 -1000

    Add watch list functionalilty

--- routes/index.js
+++ routes/index.js
@@ -11,21 +11,57 @@ router.get('/', asyncHandler(async (req, res, next) => {
 
 /* GET shows/:id */
 router.get('/shows/:id(\\d+)', asyncHandler(async (req, res) => {
-  const id = parseInt(req.params.id, 10);
-  const show = await db.Show.findByPk(id);
+  const showId = parseInt(req.params.id, 10);
+  const show = await db.Show.findByPk(showId);
   const reviews = await db.Review.findAll({
-    where: { showId: id },
-    include: [{
-      model: db.User,
-      as: 'User'
-    }]
+    where: { showId },
+    include: [{ model: db.User, as: 'User' }]
+  });
+
+  const watched = await db.WatchedList.findOne({
+    where: {
+      showId,
+      userId: res.locals.user.id
+    }
+  });
+
+  const wantToWatch = await db.WantToWatchList.findOne({
+    where: {
+      showId,
+      userId: res.locals.user.id
+    }
   });
 
   res.render('show', {
     title: show.title,
     show,
-    reviews
-  })
+    reviews,
+    watched,
+    wantToWatch
+  });
 }));
+
+/* Adds show to watched list */
+router.post('/shows/:id/watched', asyncHandler(async (req, res) => {
+  const showId = parseInt(req.params.id, 10);
+  const userId = res.locals.user.id;
+  await db.WatchedList.create({
+    showId,
+    userId
+  })
+  res.redirect(`/shows/${showId}`)
+}));
+
+/* Adds show to want to watch list */
+router.post('/shows/:id/want-to-watch', asyncHandler(async (req, res) => {
+  const showId = parseInt(req.params.id, 10);
+  const userId = res.locals.user.id;
+  await db.WantToWatchList.create({
+    showId,
+    userId
+  })
+  res.redirect(`/shows/${showId}`)
+}));
+
 
 module.exports = router;
