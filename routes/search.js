0a82e097077170d5a34cb97733b73ad4690981be
Alejandro Ventura De Moya <alexventura@gmail.com>
2021-06-06 00:52:40 -0500

    debug search fucntionality

--- routes/search.js
+++ routes/search.js
@@ -2,16 +2,22 @@ const express = require('express');
 const { csrfProtection, asyncHandler } = require('./utils');
 const { requireAuth } = require("../auth");
 const db = require('../db/models');
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
 
 const router = express.Router();
 
 router.post("/search", requireAuth, csrfProtection, asyncHandler(async (req, res, next) => {
+  const referer = req.get("Referer");
   const { q } = req.body;
   const show = await db.Show.findOne({
     where: {
-      title: q
+      title: { [Op.iLike]: q }
     }
   });
+  if (show === null) {
+    res.redirect(referer);
+  }
   res.redirect(`/shows/${show.id}`)
 }));
 
